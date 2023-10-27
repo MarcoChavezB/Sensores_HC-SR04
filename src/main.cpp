62c5bb67cf1e7c3a1a310ededb1d965c39318fc3
Marco <marco1102004@gmail.com>
2023-10-26 19:21:34 -0600

    empezando

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,29 @@
 #include <Arduino.h>
-
-// put function declarations here:
-int myFunction(int, int);
+int sensores[3][4] = {{13, 12, 14, 27}, {15, 2, 4, 5}};
+int cantSensores = 2;
+int cantPinSensor = 4;
+long distancia;
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(9600);
+  for(int i = 0; i < cantSensores; i++){
+    for(int j = 0; j < cantPinSensor; j++){
+      pinMode(sensores[i][j], OUTPUT);
+    }
+  }
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  calcularDistancia();
+  delay(1000);
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+void calcularDistancia(){
+ for(int i = 0; i < cantSensores; i++){
+    for(int j = 0; j < cantPinSensor; j++){
+      long duracion = pulseIn(j, HIGH);
+      distancia = duracion / 58;
+      Serial.println(distancia);
+    }
+  } 
 }
